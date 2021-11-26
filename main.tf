06dbd131d46f6a367fe118485de5a4b7c6e9b6e2
Pushpendra <psarwa.67@outlook.com>
2021-11-26 09:36:38 +0000

    added terraform here

---
+++ main.tf
@@ -0,0 +1,12 @@
+providers "aws" {
+	region = "ap-south-1"
+}
+
+resource "aws_instance" "push" {
+	ami = "ami-026f33d38b6410e30"
+	instance_type = "t2.micro"
+	tags = {
+		Name = "terrapush"
+		Env = Terra"
+	}
+}
